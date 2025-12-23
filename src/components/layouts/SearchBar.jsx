3fb35aa3414e6460c3b9645f61b9bcdfc24a97a6
Sijan666 <666majharulislam@gmail.com>
2025-12-23 23:14:43 +0600

    using dropdown

--- src/components/layouts/SearchBar.jsx
+++ src/components/layouts/SearchBar.jsx
@@ -8,6 +8,7 @@ import { FaUser } from "react-icons/fa";
 import { GoTriangleDown } from "react-icons/go";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { Menu, MenuButton, MenuItems, Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
 
 const SearchBar = () => {
     return (
@@ -15,9 +16,26 @@ const SearchBar = () => {
         <div className="bg-[#F5F5F3] border border-t-[#979797] border-b-[#979797]">
             <Container className={'py-6'}>
                 <Flex className={'justify-between items-center'}>
-                    <div className="iconText flex items-center">
-                        <HugeiconsIcon icon={MenuTwoLineIcon} color='text-[#262626]' strokeWidth={3}/>
-                        <h3 className='pl-3 text-[14px] text-[#262626]'>Shop by Category</h3>
+                    <div className="iconText items-center">
+                        
+                        {/* <h3 className='pl-3 text-[14px] text-[#262626]'>Shop by Category</h3> */}
+                        <Popover className="relative ">
+                            <PopoverButton className={'ml-2 outline-none'}>
+                                <HugeiconsIcon icon={MenuTwoLineIcon} color='text-[#262626]' strokeWidth={3} className='inline-block mr-3'/> 
+                                Shop by Category
+                            </PopoverButton>
+                            <PopoverPanel anchor="bottom" className="flex flex-col bg-black p-[15px] w-[170px] text-white rounded-[20px]">
+                                <Link to={'/shop'} className='mb-2'>
+                                    Shop
+                                </Link>
+                                <Link to={'/about'} className='mb-2'>
+                                    About
+                                </Link>
+                                <Link to={'/contact'} className='mb-2'>
+                                    Contact
+                                </Link>
+                            </PopoverPanel>
+                        </Popover>
                     </div>
                     <div className="searchBar">
                         <div className="flex justify-between items-center bg-white border-gray-300 px-5 py-5 w-[600px]">
